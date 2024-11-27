1b98c14bd4e3ef2b3c6ec9af90a87c0b4f874d86
Samaali Takoua <samaali.takoua@gmail.com>
2024-11-27 14:11:50 +0100

    Modified angular.json and deleted webpack.config.json

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require('webpack');
-
-module.exports = {
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.CUSTOM_VARIABLE': JSON.stringify('Ma valeur personnalisée')
-    })
-  ]
-};
