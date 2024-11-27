df61c13ba44453f2c8b10c284e4278b9d0e8f495
Samaali Takoua <samaali.takoua@gmail.com>
2024-11-27 18:12:52 +0100

    environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://backendforum-8dpz.onrender.com/api'
 };
 
 /*
