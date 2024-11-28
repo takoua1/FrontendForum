500bbcfe84176afb354ad4aeaed109b168149f98
Samaali Takoua <samaali.takoua@gmail.com>
2024-11-28 12:42:53 +0100

    Modofied auth-interceptor

--- src/app/services/security/auth-interceptor.service.ts
+++ src/app/services/security/auth-interceptor.service.ts
@@ -1,50 +1,52 @@
+import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-
-import { catchError, switchMap } from 'rxjs/operators';
-import { AuthService } from '../auth.service';
 import { TokenStorageService } from '../token-storage.service';
+import { catchError, Observable, switchMap } from 'rxjs';
+import { AuthService } from '../auth.service';
 
-@Injectable()
-export class AuthInterceptor implements HttpInterceptor {
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthInterceptorService implements HttpInterceptor {
 
-  constructor(
-    private authService: AuthService,
-    private tokenStorageService: TokenStorageService
-  ) {}
+  constructor(private tokenStorageService: TokenStorageService, private authService: AuthService) {
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.tokenStorageService.getToken();
+   }
 
+   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // Ajouter le jeton d'accès dans les en-têtes de la requête si disponible
+    let token = this.tokenStorageService.getToken();
     if (token) {
       req = req.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`,
-        }
+        },
       });
     }
 
+    // Gérer les erreurs liées au jeton expiré
     return next.handle(req).pipe(
       catchError((error) => {
-        // Si le jeton est expiré (ou tout autre type d'erreur), rafraîchir le jeton
+        // Si l'erreur est liée à l'expiration du jeton (par exemple code 401)
         if (error.status === 401) {
           return this.authService.refreshToken().pipe(
             switchMap(() => {
-              // Rejouer la requête avec le nouveau jeton
+              // Après avoir rafraîchi le jeton, reprendre la requête avec le nouveau jeton
               const newToken = this.tokenStorageService.getToken();
               const clonedRequest = req.clone({
                 setHeaders: {
                   Authorization: `Bearer ${newToken}`,
-                }
+                },
               });
               return next.handle(clonedRequest);
             })
           );
         }
-        throw error;
+        throw error; // Relancer l'erreur si ce n'est pas un problème de jeton expiré
       })
     );
-  }
-}
+   }}
+export const authInterceptorProviders = [
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
+];
+
