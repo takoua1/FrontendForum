c0b389de1ebe655aa5229f3f62de7d305cec9950
Samaali Takoua <samaali.takoua@gmail.com>
2024-12-02 15:05:03 +0100

    Modified sidebar.css

--- src/app/shared/sidebar/sidebar.component.spec.ts
+++ src/app/shared/sidebar/sidebar.component.spec.ts
@@ -11,7 +11,7 @@ describe('SidebarComponent', () => {
       declarations: [ SidebarComponent ]
     })
     .compileComponents();
-  });
+  });                                   
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SidebarComponent);
