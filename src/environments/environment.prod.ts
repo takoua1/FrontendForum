706fe621c561485c5781604be92065be076e27fc
Samaali Takoua <samaali.takoua@gmail.com>
2025-02-06 00:48:24 +0100

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
  production: true,
-apiUrl: 'http://192.168.1.14:8084'
+apiUrl: 'https://backendforum-ovbi.onrender.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://192.168.1.14:8084'
+  apiUrl: 'https://backendforum-ovbi.onrender.com'
 };
 
 /*
